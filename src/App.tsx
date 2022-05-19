e90fd5aba8e2810dffe4f085e8a9e67e67d03737
kolson <kolson@dmsi.com>
2022-05-19 10:46:12 -0500

    get Characters from backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import CharacterList from "./components/CharacterList/CharacterList";
 import NavBar from "./components/NavBar/NavBar";
 
+const client = new ApolloClient({
+  uri: "http://localhost:8080/query",
+  cache: new InMemoryCache(),
+});
+
 const App = () => {
   return (
-    <>
+    <ApolloProvider client={client}>
       <NavBar />
       <CharacterList />
-    </>
+    </ApolloProvider>
   );
 };
 
--- src/components/CharacterList/CharacterList.tsx
+++ src/components/CharacterList/CharacterList.tsx
@@ -1,26 +1,31 @@
+import { gql, useQuery } from "@apollo/client";
 import { useState } from "react";
 import type { Character } from "../../types/Character";
 
+interface CharacterResponse {
+  characters: Character[];
+}
+
+const query = gql`
+  query Characters {
+    characters {
+      id
+      name
+      description
+      gender
+    }
+  }
+`;
+
 const CharacterList = () => {
-  const [characters, setCharacters] = useState<Character[]>([
-    //These will eventually come from the backend
-    {
-      id: 1,
-      name: "God",
-      description: "God",
-    },
-    {
-      id: 2,
-      name: "Adam",
-      description: "1st man",
-      gender: "male",
-    },
-  ]);
+  const { data, loading, error } = useQuery<CharacterResponse>(query);
+  if (loading || data == null) return <p>Loading...</p>;
+  if (error) return <pre>{error.message}</pre>;
   return (
     <div className="CharacterList">
-      {characters.map((char) => (
+      {data.characters.map((char) => (
         <div className="character" key={char.id}>
-          {char.name} : {char.description} : {char.gender || "N/A"}
+          {char.name} : {char.description} : {char.gender || "none"}
         </div>
       ))}
     </div>
